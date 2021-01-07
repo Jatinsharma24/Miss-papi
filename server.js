74a17f3cb9b1feddcac69615936ac8b07458ac91
Glitch (hello-express) <none>
2021-01-07 05:01:55 +0000

    📼🖼 Checkpoint ./server.js:25919202/87

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const discord = require("discord.js");
 const client = new discord.Client();
 const alexa = require("alexa-bot-api");
-var chatbot = new alexa("aw2plm"); //free access key for Mewtwo Machine channel (provided by CTK WARRIOR)
+var chatbot = new alexa("aw2plm"); //free access key for mewto machine provided by Nobody™
 client.on("ready", () => {
   console.log("Ready for chatting!| Bot by 0_0");
 });
